9eb041f10c6750c9bd418c83a040d01ac0f6d28e
Tom Mwambingu <[tomchom30@gmail.com]>
2025-07-21 12:07:23 +0300

    Added SQLite connection

--- utils/db.py
+++ utils/db.py
@@ -2,3 +2,10 @@ import sqlite3
 import os
 
 DB_PATH = os.path.join("data", "rental.db")
+
+def connect():
+    return sqlite3.connect(DB_PATH)
+
+def init_db():
+    conn = connect()
+    cursor = conn.cursor()
