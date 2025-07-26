668708edbb7d7aba5bcee2632cd3e23a13dc676a
Tom Mwambingu <[tomchom30@gmail.com]>
2025-07-26 18:51:44 +0300

    Added view tenants

--- models/tenant.py
+++ models/tenant.py
@@ -12,7 +12,16 @@ def add_tenant(name, phone, house_id):
 def view_tenants():
     conn = connect()
     cur = conn.cursor()
-    cur.execute("SELECT * FROM tenants")
+    cur.execute("""
+        SELECT tenants.id, tenants.name, tenants.phone, houses.house_number
+        FROM tenants
+        JOIN houses ON tenants.house_id = houses.id
+    """)
     tenants = cur.fetchall()
     conn.close()
-    return tenants
+
+    if tenants:
+        for tenant in tenants:
+            print(f"ID: {tenant[0]} | Name: {tenant[1]} | Phone: {tenant[2]} | House: {tenant[3]}")
+    else:
+        print("No tenants found.")
